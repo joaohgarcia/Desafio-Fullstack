e2188535f7061d0a40d751ddada279d7537a06c5
João Henrique Garcia <joaohgarcia@gmail.com>
2020-09-21 13:03:42 -0300

    Termino de renomeação

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CasaVA")]
+[assembly: AssemblyTitle("TemplateBootstrap")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CasaVA")]
+[assembly: AssemblyProduct("TemplateBootstrap")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
